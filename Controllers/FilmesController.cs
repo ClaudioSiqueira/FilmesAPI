5bc5a139abf3264fb361c2b5b480321b08902df4
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2021-07-09 22:40:42 -0300

    Configurando Controller

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<Filme> AdicionarFilme([FromBody] Filme filme)
+        {
+            return Ok(filme);
+        }   
+    }
+}
